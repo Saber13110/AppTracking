3f5c2dd8696a037305ec0eec75d96ca93030cb51
Saber13110 <elouakilisaber@gmail.com>
2025-06-07 10:02:58 -1000

    Add Google auth callback component and route

---
+++ Frontend/src/app/features/auth/google-callback/google-callback.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-google-callback',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './google-callback.component.html',
+  styleUrls: ['./google-callback.component.scss']
+})
+export class GoogleCallbackComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const token = params['token'];
+      if (token) {
+        localStorage.setItem('token', token);
+      }
+      this.router.navigate(['/home']);
+    });
+  }
+}
