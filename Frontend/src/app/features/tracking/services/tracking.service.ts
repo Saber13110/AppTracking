692b5fe6df3f4bc97136dbcf9cf44765dce1b879
Saber13110 <elouakilisaber@gmail.com>
2025-06-08 02:42:18 -1000

    refactor(tracking): centralize TrackingResponse

--- Frontend/src/app/features/tracking/services/tracking.service.ts
+++ Frontend/src/app/features/tracking/services/tracking.service.ts
@@ -2,14 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-import { TrackingInfo } from '../models/tracking';
-
-export interface TrackingResponse {
-  success: boolean;
-  data?: TrackingInfo;
-  error?: string;
-  metadata?: { [key: string]: any };
-}
+import { TrackingInfo, TrackingResponse } from '../models/tracking';
 
 @Injectable({
   providedIn: 'root'
@@ -63,4 +56,4 @@ export class TrackingService {
   }
 }
 
-export type { TrackingInfo } from '../models/tracking';
+export type { TrackingInfo, TrackingResponse } from '../models/tracking';
