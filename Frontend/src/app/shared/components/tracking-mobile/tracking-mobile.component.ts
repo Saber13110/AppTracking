2efc054d1e939f4dae5e23d243a11f3747aad864
Saber13110 <elouakilisaber@gmail.com>
2025-06-08 04:00:35 -1000

    feat(frontend): add tracking and footer components

---
+++ Frontend/src/app/shared/components/tracking-mobile/tracking-mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tracking-mobile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tracking-mobile.component.html',
+  styleUrls: ['./tracking-mobile.component.scss']
+})
+export class TrackingMobileComponent {}
