1046c896886475bd094a148c01a879854cb8ae1f
Saber13110 <elouakilisaber@gmail.com>
2025-06-08 12:36:49 -1000

    Add news service and fetch article in detail component

--- Frontend/src/app/features/news/news-detail.component.ts
+++ Frontend/src/app/features/news/news-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { NewsService } from './services/news.service';
+import { NewsArticle } from './models/news';
 
 @Component({
   selector: 'app-news-detail',
@@ -12,17 +14,26 @@ import { Subscription } from 'rxjs';
 })
 export class NewsDetailComponent implements OnInit, OnDestroy {
   slug: string | null = null;
+  article: NewsArticle | null = null;
   private paramsSub: Subscription | null = null;
+  private articleSub: Subscription | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private newsService: NewsService) {}
 
   ngOnInit() {
     this.paramsSub = this.route.params.subscribe(params => {
       this.slug = params['slug'];
+      if (this.slug) {
+        this.articleSub?.unsubscribe();
+        this.articleSub = this.newsService
+          .getArticle(this.slug)
+          .subscribe(article => (this.article = article));
+      }
     });
   }
 
   ngOnDestroy() {
     this.paramsSub?.unsubscribe();
+    this.articleSub?.unsubscribe();
   }
 }
