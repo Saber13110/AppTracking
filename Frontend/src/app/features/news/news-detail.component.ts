f78561860fbbe0c3f604a86b589e386354986700
Saber13110 <elouakilisaber@gmail.com>
2025-06-08 04:38:04 -1000

    fix: unsubscribe from route params

--- Frontend/src/app/features/news/news-detail.component.ts
+++ Frontend/src/app/features/news/news-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-news-detail',
@@ -9,14 +10,19 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './news-detail.component.html',
   styleUrls: ['./news-detail.component.scss']
 })
-export class NewsDetailComponent implements OnInit {
+export class NewsDetailComponent implements OnInit, OnDestroy {
   slug: string | null = null;
+  private paramsSub: Subscription | null = null;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.paramsSub = this.route.params.subscribe(params => {
       this.slug = params['slug'];
     });
+  }
+
+  ngOnDestroy() {
+    this.paramsSub?.unsubscribe();
   }
 }
